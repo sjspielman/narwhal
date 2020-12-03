2bceb431660c3ec42090678a3367071a6dd2ca75
Stephanie Spielman <spielman@rowan.edu>
2020-12-03 19:38:36 +0000

    Added a title to the plot

--- plot.R
+++ plot.R
@@ -1,4 +1,6 @@
 library(tidyverse)
 library(palmerpenguins)
 
-ggplot(penguins, aes(x = bill_depth_mm)) + geom_histogram()
+ggplot(penguins, aes(x = bill_depth_mm)) + 
+  geom_histogram() + 
+  labs(title = "a histogram!")
